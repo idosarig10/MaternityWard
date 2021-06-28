1509d61e275cf1880bd33a6444681cf327596ff1
ido sarig <idosarig10@gmail.com>
2021-06-28 16:05:31 +0300

    Add CleanersShiftManager and MultiCleaners

---
+++ Workers/CleanersShiftManager.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MaternityWard.Tables;
+
+namespace MaternityWard.RankSalaries
+{
+    class CleanersShiftManager : IWorker
+    {
+        public DecisionTaker DecisionTaker { set; get; }
+        public SqliteDbContext Db { set; get; }
+        public string WorkerId { set; get; }
+        public CleanersShiftManager(SqliteDbContext db, string workerId)
+        {
+            this.DecisionTaker = new DecisionTaker(db, workerId);
+            this.Db = db;
+            this.WorkerId = workerId;
+        }
+        public float Calculate()
+        {
+            return this.DecisionTaker.Calculate(0);
+        }
+        public void AddToDb(float hourlyRate, float monthActualWorkHours = 0)
+        {
+            this.Db.Workers.Add(new Worker { Id = this.WorkerId, WorkerType = this.GetType().Name});
+            this.Db.HourlyRates.Add(new HourlyRate { Id = this.WorkerId, Value = hourlyRate });
+            this.Db.MonthActualWorkHours.Add(new MonthActualWorkHours { WorkerId = this.WorkerId, Hours = monthActualWorkHours });
+            this.Db.SaveChanges();
+        }
+    }
+}
