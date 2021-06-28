19646f20a9ce11c20e7acbe266ca4ef68c238442
ido sarig <idosarig10@gmail.com>
2021-06-28 14:57:48 +0300

    update Db add sample data

--- Program.cs
+++ Program.cs
@@ -9,6 +9,7 @@ namespace MaternityWard
         {
              using (var db = new SqliteDbContext())
              {
+
                 //Guid x = new Guid("6798c85d-9ba1-44a4-92b2-6be8a4a26aa5");
                 //Senior senior = new Senior(db, x);
                 //Worker worker = new Worker{ Id=x, HourlyWage=40, WorkerType="Cleaner"};
--- RankSalaries/Junior.cs
+++ RankSalaries/Junior.cs
@@ -2,10 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using MaternityWard.Tables;
 
 namespace MaternityWard.RankSalaries
 {
-    class Junior: ISalary
+    class Junior : ISalary
     {
         public SqliteDbContext Db { set; get; }
         public string WorkerId { set; get; }
