49d4df030291817754c244d007c2a5c55b03c2cd
ido sarig <idosarig10@gmail.com>
2021-06-29 12:33:04 +0300

    Add main loop in program

--- Program.cs
+++ Program.cs
@@ -13,11 +13,30 @@ namespace MaternityWard
              {
 
                 Assembly assembly = Assembly.GetExecutingAssembly();
-                ConsoleApplicationInterface.CalculateSalary(db, assembly);
-                //while (true)
-                //{ 
-                
-                //}
+                string command;
+                while (true)
+                {
+                    Console.WriteLine("what whould you like to do?");
+                    Console.WriteLine("1. add worker");
+                    Console.WriteLine("2. report worker work time");
+                    Console.WriteLine("3. calculate worker salary");
+                    Console.WriteLine("everything else to exit");
+                    command = Console.ReadLine();
+                    switch (command)
+                    {
+                        case "1":
+                            ConsoleApplicationInterface.AddWorker(db, assembly);
+                            break;
+                        case "2":
+                            ConsoleApplicationInterface.AddWorkTime(db);
+                            break;
+                        case "3":
+                            ConsoleApplicationInterface.CalculateSalary(db, assembly);
+                            break;
+                        default:
+                            return;
+                    }
+                }
             };
         }
     }
