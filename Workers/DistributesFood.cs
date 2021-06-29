d453ea964bb29e02ab0a219f34d1a6336d250bb2
ido sarig <idosarig10@gmail.com>
2021-06-29 10:00:31 +0300

    Add new Workers

---
+++ Workers/DistributesFood.cs
@@ -0,0 +1,35 @@
+﻿using MaternityWard.Tables;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MaternityWard.RankSalaries
+{
+    class DistributesFood: IWorker
+    {
+        public SqliteDbContext Db { set; get; }
+        public string WorkerId { set; get; }
+
+        public Junior Junior;
+        public DistributesFood(SqliteDbContext db, string workerId)
+        {
+            this.Junior = new Junior(db, workerId);
+            this.Db = db;
+            this.WorkerId = WorkerId;
+        }
+        public float Calculate()
+        {
+            return this.Junior.Calculate(0);
+        }
+
+        public void AddToDb(float hourlyRate, float monthWorkHours, float monthActualWorkHours = 0)
+        {
+            this.Db.Workers.Add(new Worker { Id = this.WorkerId, WorkerType = this.GetType().Name });
+            this.Db.HourlyRates.Add(new HourlyRate { Id = this.WorkerId, Value = hourlyRate });
+            this.Db.MonthWorkHours.Add(new MonthWorkHours { WorkerId = this.WorkerId, Hours = monthWorkHours });
+            this.Db.MonthActualWorkHours.Add(new MonthActualWorkHours { WorkerId = this.WorkerId, Hours = monthActualWorkHours });
+            this.Db.SaveChanges();
+        }
+    }
+}
---
+++ Workers/HeadOfAdministration.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MaternityWard.Tables;
+
+namespace MaternityWard.RankSalaries
+{
+    class HeadOfAdministration : IWorker
+    {
+        public DecisionTaker DecisionTaker;
+        public Manager Manager;
+        public SqliteDbContext Db { set; get; }
+        public string WorkerId { set; get; }
+        public HeadOfAdministration(SqliteDbContext db, string workerId)
+        {
+            this.DecisionTaker = new DecisionTaker(db, workerId);
+            this.Manager = new Manager(db, workerId);
+            this.Db = db;
+            this.WorkerId = workerId;
+        }
+        public float Calculate()
+        {
+            return this.Manager.Calculate(this.DecisionTaker.Calculate(0));
+        }
+        public void AddToDb(float hourlyRate, float monthWorkHours, float monthActualWorkHours = 0)
+        {
+            this.Db.Workers.Add(new Worker { Id = this.WorkerId, WorkerType = this.GetType().Name});
+            this.Db.HourlyRates.Add(new HourlyRate { Id = this.WorkerId, Value = hourlyRate });
+            this.Db.MonthWorkHours.Add(new MonthWorkHours { WorkerId = this.WorkerId, Hours = monthWorkHours });
+            this.Db.MonthActualWorkHours.Add(new MonthActualWorkHours { WorkerId = this.WorkerId, Hours = monthActualWorkHours });
+            this.Db.SaveChanges();
+        }
+    }
+}
